0e5bb0f30b5852f9baa15ce3919fd3a194961dd6
Diakho Camara <diakho.camara@gmail.com>
2022-12-09 11:09:19 +0100

    Create theme header

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -4,8 +4,8 @@
 ** activation theme
 **/
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 20 );
 
 function theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ wp_enqueue_style( 'hello-elementor-child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
